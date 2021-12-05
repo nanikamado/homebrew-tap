9a744584cd7ba91c38c298ec02d3ca72a8070d0b
Mado Nanika <nanikamado@gmail.com>
2021-12-05 18:45:03 +0900

    Fix versions

--- Formula/tex-html.rb
+++ Formula/tex-html.rb
@@ -1,11 +1,11 @@
 class TexHtml < Formula
-  url "https://github.com/nanikamado/tex-html.git", :branch => "main"
-  version "0.1.0"
+  url "https://github.com/nanikamado/tex-html.git", revision: "d600393ccbd662afbea52951d39e4de38b946876"
+  version "0.2.0"
   license "MIT"
 
   depends_on "ruby"
-  depends_on "pandoc"
-  depends_on "pandoc-crossref"
+  depends_on "pandoc" => "2.16.1"
+  depends_on "pandoc-crossref" => "0.3.12.1"
   depends_on "nanikamado/tap/pandoc-asciimath-filter"
   depends_on "nanikamado/tap/pandoc-katex"
 
